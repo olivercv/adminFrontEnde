6a5581a8d2d087a2bca57c186b855237ceb4ce1f
agmontan <carlos05.ale@gmail.com>
2020-10-19 16:44:57 -0400

    convocatory

--- src/app/pages/convocatory/convocatory-form.component.ts
+++ src/app/pages/convocatory/convocatory-form.component.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { ModalFileUploadService } from '../../components/modal-file-upload/modal-file-upload.service';
-import { MatDialog } from '@angular/material/dialog';
-import { NgForm } from '@angular/forms';
-import { ModalFileUploadComponent } from '../../components/modal-file-upload/modal-file-upload.component';
-import { Convocatory } from '../../models/convocatory.model';
-import { ConvocatoryService } from '../../services/convocatory/convocatory.service';
-import { Doc } from '../../models/doc.model';
-
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { ModalFileUploadService } from "../../components/modal-file-upload/modal-file-upload.service";
+import { MatDialog } from "@angular/material/dialog";
+import { NgForm } from "@angular/forms";
+import { ModalFileUploadComponent } from "../../components/modal-file-upload/modal-file-upload.component";
+import { Convocatory } from "../../models/convocatory.model";
+import { ConvocatoryService } from "../../services/convocatory/convocatory.service";
+import { Doc } from "../../models/doc.model";
 
 @Component({
-  selector: 'app-convocatory-form',
-  templateUrl: './convocatory-form.component.html',
-  styleUrls: []
+  selector: "app-convocatory-form",
+  templateUrl: "./convocatory-form.component.html",
+  styleUrls: [],
 })
 export class ConvocatoryFormComponent implements OnInit {
-
-  convocatory: Convocatory = new Convocatory("","",new Date(),new Date(),new Date(),new Date(),"","");
-  doc: Doc = new Doc("","","","","","");
-  docs: Doc [] = [];
-  documents: Doc [] = [];
-  iconFile = '../assets/images/icon/file.png';
+  convocatory: Convocatory = new Convocatory(
+    "",
+    "",
+    new Date(),
+    new Date(),
+    new Date(),
+    new Date(),
+    "",
+    ""
+  );
+  doc: Doc = new Doc("", "", "", "", "", "");
+  documents: Doc[] = [];
+  iconFile = "../assets/images/icon/file.png";
   constructor(
     public _convocatoryService: ConvocatoryService,
     public router: Router,
@@ -28,139 +34,103 @@ export class ConvocatoryFormComponent implements OnInit {
     public modalUploadFileService: ModalFileUploadService,
     public dialog: MatDialog
   ) {
-    activatedRoute.params.subscribe(params=>{
-      let id = params['id'];
-      if( id !== 'nuevo' ){
+    activatedRoute.params.subscribe((params) => {
+      let id = params["id"];
+      if (id !== "nuevo") {
         this.cargarConvocatory(id);
       }
-    })
-   }
+    });
+  }
 
   ngOnInit(): void {
-    this.cargarDocs();
+    if (this.convocatory._id) {
+      this.cargarConvocatory(this.convocatory._id);
+    }
   }
 
-
-
-  cargarConvocatory( id: string ) {
-    this._convocatoryService.cargarConvocatory( id )
-          .subscribe( convocatory => {
-
-            console.log( convocatory );
-            this.convocatory = convocatory;
-            this.docs = convocatory.docs;
-          });
+  cargarConvocatory(id: string) {
+    this._convocatoryService.cargarConvocatory(id).subscribe((convocatory) => {
+      console.log(convocatory);
+      this.convocatory = convocatory;
+      this.documents = convocatory.docs;
+      console.log(this.documents);
+    });
   }
 
-  guardarConvocatory( f: NgForm ) {
+  guardarConvocatory(f: NgForm) {
+    console.log(f.valid);
+    console.log(f.value);
 
-    console.log( f.valid );
-    console.log( f.value );
-
-    if ( f.invalid ) {
+    if (f.invalid) {
       return;
     }
 
-    this._convocatoryService.guardarConvocatory( this.convocatory )
-            .subscribe( convocatory => {
-
-              this.convocatory._id = convocatory._id;
-
-              this.router.navigate(['/convocatory', convocatory._id ]);
-
-            });
-
+    this._convocatoryService
+      .guardarConvocatory(this.convocatory)
+      .subscribe((convocatory) => {
+        this.convocatory._id = convocatory._id;
+        this.cargarConvocatory(this.convocatory._id);
+        this.router.navigate(["/convocatory", convocatory._id]);
+      });
   }
 
-  guardarDoc(  id: string ) {
-
-    this.doc.titulo = 'Nuevo Documento';
+  guardarDoc(id: string) {
+    this.doc.titulo = "Nuevo Documento";
     this.doc.convocatory = id;
     console.log(this.doc);
-    if ( !id ) {
+    if (!id) {
       return;
     }
 
-    this._convocatoryService.guardarDoc( this.doc )
-            .subscribe( doc => {
+    this._convocatoryService.guardarDoc(this.doc).subscribe((doc) => {
+      this.doc._id = doc._id;
 
-              this.doc._id = doc._id;
+      this.cargarConvocatory(this.convocatory._id);
+      //this.router.navigate(['/convocatory', this.convocatory._id ]);
+    });
+  }
 
-              this.cargarConvocatory(this.convocatory._id);
+  guardarDocumento(doc: Doc) {
+    this._convocatoryService
+      .actualizarDoc(doc)
+      .subscribe(() => this.cargarConvocatory(this.convocatory._id));
+  }
 
+  deleteDoc(doc: Doc) {
+    this._convocatoryService
+      .deleteDoc(doc._id)
+      .subscribe(() => this.cargarConvocatory(this.convocatory._id));
+  }
 
-            });
+  changeFile(id: string) {
+    this.dialog.open(ModalFileUploadComponent);
+    this.modalUploadFileService.showModal("docs", id);
 
-}
+    this.modalUploadFileService.modalNotification.subscribe((resp: any) => {
+      //console.log('doc archivo',resp);
+      this.cargarConvocatory(this.convocatory._id);
+      this.changeIcon(resp.doc.sfile);
+    });
+  }
 
-guardarDocumento( doc: Doc) {
+  changeIcon(sfile) {
+    if (!sfile) {
+      this.iconFile = "../assets/images/icon/add-file.png";
+      return;
+    }
+    const ext = sfile.split(".");
+    //console.log('extencion', ext);
 
-  this._convocatoryService.actualizarDoc( doc )
-          .subscribe( //() => this.cargarDocs()
-
-          );
-
-}
-
-deleteDoc( doc: Doc ) {
-
-  this._convocatoryService.deleteDoc( doc._id )
-          .subscribe ( () => this.cargarConvocatory(this.convocatory._id) );
-
-}
-
-cargarDocs() {
-  this._convocatoryService.cargarDocs()
-          .subscribe( docs => {
-            this.docs = docs;
-            this.filtrarDocs();
-          });
-}
-
-filtrarDocs(){
-  this.documents = [];
-  this.docs.forEach(document => {
-
-    if (document.convocatory === this.convocatory._id ) {
-      this.documents.push(document);
+    if (ext[1] === "pdf") {
+      this.iconFile = "../assets/images/icon/pdf.png";
     }
 
-  });
-}
+    if (ext[1] === "docx" || ext[1] === "doc") {
+      this.iconFile = "../assets/images/icon/doc.png";
+    }
 
-changeFile(id: string) {
-  this.dialog.open(ModalFileUploadComponent);
-  this.modalUploadFileService.showModal( 'docs', id );
-
-  this.modalUploadFileService.modalNotification
-              .subscribe( (resp: any) => {
-                //console.log('doc archivo',resp);
-                this.cargarConvocatory(this.convocatory._id);
-                this.changeIcon(resp.doc.sfile);
-              });
-}
-
-changeIcon(sfile) {
-
-  if (!sfile) {
-    this.iconFile = '../assets/images/icon/add-file.png';
-    return;
+    if (ext[1] === "xls" || ext[1] === "xlsx" || ext[1] === "csv") {
+      this.iconFile = "../assets/images/icon/xls.png";
+    }
   }
-  const ext = sfile.split('.');
-  //console.log('extencion', ext);
-
-  if (ext[1] === 'pdf') {
-    this.iconFile = '../assets/images/icon/pdf.png';
-  }
-
-  if (ext[1] === 'docx' || ext[1] === 'doc') {
-    this.iconFile = '../assets/images/icon/doc.png';
-  }
-
-  if (ext[1] === 'xls' || ext[1] === 'xlsx' || ext[1] === 'csv') {
-    this.iconFile = '../assets/images/icon/xls.png';
-  }
-
-}
-
 }
